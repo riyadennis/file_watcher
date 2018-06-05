bc3443a2ff7bb67497d03929296890f0cc6e08d8
Riya Dennis <riyacv@gmail.com>
2018-06-05 23:39:10 +0100

    Added redis wrapper into dependency

--- services/watcher.go
+++ services/watcher.go
@@ -2,9 +2,11 @@ package services
 
 import (
 	"github.com/radovskyb/watcher"
-	"fmt"
 	"time"
 	"github.com/sirupsen/logrus"
+	"github.com/go-redis/redis"
+	"github.com/riyadennis/redis-wrapper"
+	"github.com/satori/go.uuid"
 )
 
 type WatchedFile struct {
@@ -17,12 +19,14 @@ func WatchFolder(folder string, watcher *watcher.Watcher) {
 	if err := watcher.Add(folder); err != nil {
 		logrus.Fatalln(err)
 	}
-	// Print a list of all of the files and folders currently
-	// being watched and their paths.
-	files := GetWatchedFiles(watcher)
-	for _, file := range files {
-		fmt.Printf("File Name: %s \n", file.Name)
+	c := redis_wrapper.Client{}
+	client, err := c.Create()
+	if err != nil {
+		logrus.Error(err.Error())
 	}
+
+	AddFileNameToClient(client.RedisClient, watcher)
+
 	// Trigger 2 events after watcher started.
 	go func() {
 		watcher.Wait()
@@ -32,7 +36,16 @@ func WatchFolder(folder string, watcher *watcher.Watcher) {
 		logrus.Fatalln(err)
 	}
 }
-
+func AddFileNameToClient(client *redis.Client, watcher *watcher.Watcher) {
+	files := GetWatchedFiles(watcher)
+	for _, file := range files {
+		indexName := uuid.NewV4().String()
+		err := client.Set(indexName, file.Path, 0).Err()
+		if err != nil {
+			logrus.Error(err.Error())
+		}
+	}
+}
 func GetWatchedFiles(watcher *watcher.Watcher) ([]WatchedFile) {
 	files := make([]WatchedFile, 0)
 	// Print a list of all of the files and folders currently
